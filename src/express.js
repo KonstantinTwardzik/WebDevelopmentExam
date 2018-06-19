5b6c98afbf6592b15509d6d5ccbbc20658a0dcbc
Konstantin <twardzk@hochschule-trier.de>
2018-06-19 14:01:05 +0200

    added express server

--- src/express.js
+++ src/express.js
@@ -1,19 +1,25 @@
 const express = require("express");
 let server = express();
+var path = require("path");
 
-server.get("/", (request, response) => {
-    response.send("Hello, World!");
-});
+let port = process.argv[2];
+
+server.use(express.static(__dirname + "/public"));
 
 server.get("/json", (request, response) => {
-    response.json({
-        message: "Hello, World!",
-        success: true
-    });
+	response.json({
+		message: "Hello, World!",
+		success: true
+	});
 });
 
 server.get("/XXX", (request, response) => {
-    response.sendStatus(404);
+	response.sendStatus(404);
 });
 
-server.listen(8080);
+if (port) {
+	server.listen(port, () => console.log("listening on port " + port));
+}
+else {
+	server.listen(8080, () => console.log("listening on port 8080"));
+}
