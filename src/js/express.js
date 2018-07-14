0628716a2fac25bc659299e88ba53c247957bf95
Thomas Laux <lauxt@hochschule-trier.de>
2018-07-14 16:21:49 +0200

    express Commentary

--- src/js/express.js
+++ src/js/express.js
@@ -59,7 +59,7 @@ server.delete("", (req, res) => {
 	});
 });
 
-//delete a object from database
+//delete an object from database
 server.delete("/deleteMeeting", (request, response) => {
 	let id = parseInt(request.query.id);
 	let start = 0;
@@ -78,26 +78,21 @@ server.delete("/deleteMeeting", (request, response) => {
 		response.sendStatus(404);
 		return;
 	}
-	//iterate through whole db
+	//iterate through whole db and find object with appropriate id
 	for (let i = 0; i < db.meetings.length; i++) {
-		//find that one meeting with the id to look for
 		if (parseInt(db.meetings[i].id) === id) {
-			//splice by 1 (delete) the meeting in index i
+			//splice by 1 (delete) the meeting on index i
 			db.meetings.splice(i, 1);
 			makePersistent();
-			//create empty array for returning to client
 			var responseArray = [];
-			//fill that array with all meetings from start to end
 			for (let index = start; index < end; index++) {
 				responseArray.push(db.meetings[index]);
 			}
-			//sends the new created array as string to the server
 			response.json(responseArray);
-			//and returns
 			return;
 		}
 	}
-	//in case the return statement is not reached, send a 404
+	//in case the return statement is not reached, send 404
 	response.sendStatus(404);
 });
 
@@ -107,7 +102,7 @@ server.put("/editMeeting", (request, response) => {
 	let name = request.query.name;
 	let date = request.query.date;
 	let coordinates = [];
-	//must be split into two separate values for longitude and langitude, separated by a comma
+	//parseFloat doesn't work on coordinates -> separating
 	coordinates[0] = parseFloat(request.query.coordinates.split(",")[0]);
 	coordinates[1] = parseFloat(request.query.coordinates.split(",")[1]);
 	let location = request.query.location;
@@ -122,31 +117,28 @@ server.put("/editMeeting", (request, response) => {
 		coordinates: coordinates,
 		objects: objects
 	};
-	//adds object on the right index
+	//overwrites old meeting with the just created one
 	for (let i = 0; i < db.meetings.length; i++) {
 		if (parseInt(db.meetings[i].id) === id) {
 			db.meetings[i] = objectToAdd;
 		}
 	}
 	makePersistent();
-	//sends the addedObject to the client
 	response.json(objectToAdd);
 });
 
 function makePersistent() {
-	//writes into ./meetings.json the JSON.stringified value of db to make the db persistent
+	//overwrites the existing db with the edited one to be persistent on server side
 	fs.writeFile("./meetings.json", JSON.stringify(db), (err) => {
 		if (err) {
-			console.log("The file has been saved!");
+			console.log("The file hasn't been saved!");
 			throw err;
 		}
 	});
 }
 
 server.post("/addNewMeeting", (request, response) => {
-	//pretty much the same as if is in editMeeting
-	//always add a new meeting to the end of the JSON data
-	//getting the last items id in db.meetings and add 1 for the next one that will be added now
+	//always add a new meeting to the end of the JSON data with the consecutive id
 	let id = parseInt(db.meetings[db.meetings.length - 1].id + 1);
 	let name = request.query.name;
 	let date = request.query.date;
